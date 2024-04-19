889b8ab098a9b4a4b1ca1cddd1c748aadc996ace
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2024-04-19 10:04:13 -0400

    posthog updates

--- src/components/PostHog.tsx
+++ src/components/PostHog.tsx
@@ -4,10 +4,15 @@ import { PostHogProvider } from 'posthog-js/react'
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: 'https://ccnyb.us/ingest',
+    api_host: '/ingest',
     ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   })
 }
+
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+  if (process.env.NODE_ENV === 'production') {
+    return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+  } else {
+    return { children }
+  }
 }
