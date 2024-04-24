cbe9539ab15298de7280851d9888e710cfb57ca5
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2024-04-24 11:44:20 -0400

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,13 +9,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: "https://ccnyb.us/125",
+      url: "https://ccnyb.us/stop/125",
       lastModified: new Date(),
       changeFrequency: "always",
       priority: 0.8,
     },
     {
-      url: "https://ccnyb.us/145",
+      url: "https://ccnyb.us/stop/145",
       lastModified: new Date(),
       changeFrequency: "always",
       priority: 0.8,
