96eb4909b89a8c6f69abd692462735f12d2c7252
Daniel Chen <113078548+dchen024@users.noreply.github.com>
2024-09-02 23:53:32 -0400

    chore: add 145 not working on frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,71 +1,76 @@
-import type { Metadata } from "next";
-export const dynamic = "force-dynamic";
+import type { Metadata } from 'next';
+export const dynamic = 'force-dynamic';
 
-import ImageCarousel from "@/components/ImageCarousel";
+import ImageCarousel from '@/components/ImageCarousel';
 
 export const metadata: Metadata = {
-  title: "CCNY Bus Tracker",
-  description: "Never guess when the shuttle bus is coming again.",
+  title: 'CCNY Bus Tracker',
+  description: 'Never guess when the shuttle bus is coming again.',
   keywords: [
-    "CCNY",
-    "ccny",
-    "city college",
-    "city college of new york",
-    "shuttle",
-    "bus",
-    "shuttle bus",
-    "tracker",
-    "campus",
-    "transportation",
-    "schedule",
-    "route",
-    "live",
-    "real-time",
-    "map",
-    "GPS",
-    "location",
-    "service",
-    "timetable",
-    "stop",
-    "arrival",
-    "departure",
-    "status",
-    "update",
-    "mobile",
-    "students",
-    "faculty",
-  ]
+    'CCNY',
+    'ccny',
+    'city college',
+    'city college of new york',
+    'shuttle',
+    'bus',
+    'shuttle bus',
+    'tracker',
+    'campus',
+    'transportation',
+    'schedule',
+    'route',
+    'live',
+    'real-time',
+    'map',
+    'GPS',
+    'location',
+    'service',
+    'timetable',
+    'stop',
+    'arrival',
+    'departure',
+    'status',
+    'update',
+    'mobile',
+    'students',
+    'faculty',
+  ],
 };
 
 export default async function Home() {
   return (
-    <div className="pb-8">
-
-      <div className="mt-4 flex flex-col gap-8">
+    <div className='pb-8'>
+      <div className='mt-4 flex flex-col gap-8'>
         <div>
-          <div className="flex gap-4 items-baseline">
-            <div className="text-2xl font-bold">125</div>
+          <div className='flex gap-4 items-baseline'>
+            <div className='text-2xl font-bold'>125</div>
           </div>
-          <ImageCarousel stop="125" />
+          <ImageCarousel stop='125' />
+        </div>
+
+        {/* if 145 camera working frontend code */}
+        {/* <div>
+          <div className='flex gap-4 items-baseline'>
+            <div className='text-2xl font-bold'>145</div>
+          </div>
+          <ImageCarousel stop='145' />
+        </div> */}
+
+        {/* 145 camera not working */}
+        <div>
+          <div className='flex gap-4 items-baseline'>
+            <div className='text-2xl font-bold'>145 [not working]</div>
+          </div>
+          DOT switched camera direction 😡
         </div>
 
         <div>
-          <div className="flex gap-4 items-baseline">
-            <div className="text-2xl font-bold">145</div>
-          </div>
-          <ImageCarousel stop="145" />
-        </div>
-
-        <div>
-          <div className="flex gap-4 items-baseline">
-            <div className="text-2xl font-bold">nac/marshak</div>
+          <div className='flex gap-4 items-baseline'>
+            <div className='text-2xl font-bold'>nac/marshak</div>
           </div>
           support coming soon
         </div>
       </div>
-
-
-
     </div>
   );
 }
