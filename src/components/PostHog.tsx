506581209e318b20c90a16985b26a014af41667c
Daniel Chen <113078548+dchen024@users.noreply.github.com>
2024-09-02 21:20:38 -0400

    fix: fix posthog env name

--- src/components/PostHog.tsx
+++ src/components/PostHog.tsx
@@ -1,18 +1,18 @@
-'use client'
-import posthog from 'posthog-js'
-import { PostHogProvider } from 'posthog-js/react'
+'use client';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 
 if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_API_KEY!, {
     api_host: '/ingest',
     ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  })
+  });
 }
 
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
   if (process.env.NODE_ENV === 'production') {
-    return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+    return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
   } else {
-    return <>{children}</>
+    return <>{children}</>;
   }
 }
